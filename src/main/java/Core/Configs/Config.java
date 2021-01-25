42920e2d1a905ae5ccae9e79930b01c21c3f0c51
WolfgangBischoff <wolfgang.bischoff@outlook.at>
2021-01-25 22:38:54 +0100

    new sprites

--- src/main/java/Core/Configs/Config.java
+++ src/main/java/Core/Configs/Config.java
@@ -11,7 +11,7 @@ public class Config
     //General
     public static final Boolean DEBUG_ACTORS = false;
     public static final Boolean DEBUG_BLOCKER = false;
-    public static final Boolean DEBUG_NO_WALL = true;
+    public static final Boolean DEBUG_NO_WALL = false;
     public static final Boolean DEBUG_MOUSE_ANALYSIS = false;
     public static final Boolean DEBUG_ALL_TEXTBOX_OPTIONS_VISIBLE = false;
     public static final Boolean DEBUG_CONSOLE = false;
